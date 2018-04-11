def2eb9bf5efb0410d2bb5da4650b57b5a035178
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2018-04-11 14:09:18 +0200

    Server: take parameters from env and pass them as extra vars

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
 from ansible_wrapper import PlaybookRunner
 from flask import Flask, jsonify, abort, make_response, request
+from os import environ
 
 app = Flask(__name__)
+
+# environ('GITHUB_API_KEY')
+GITHUB_API_KEY = environ.get('GITHUB_API_KEY')
+GITHUB_ORG = environ.get('GITHUB_ORG')
+GITHUB_TEAM = environ.get('GITHUB_TEAM')
 
 
 @app.route('/', methods=['POST'])
 def refresh():
     runner = PlaybookRunner(
         playbook='fetch_keys.yml',
+        extra_vars={
+            "github_api_key": GITHUB_API_KEY,
+            "github_org": GITHUB_ORG,
+            "github_team": GITHUB_TEAM
+        },
         verbosity=0
     )
     stats = runner.run()
