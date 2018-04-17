3a29b27d2925f0ed2181eb19cec3ab5c65eb7671
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2018-04-17 11:23:33 +0200

    Server: add debugging log

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from ansible_wrapper import PlaybookRunner
 from flask import Flask, jsonify, abort, make_response
 from os import environ
+
+from ansible_wrapper import PlaybookRunner
+from log import log
+import logging
 
 app = Flask(__name__)
 
@@ -9,6 +12,13 @@ GITHUB_API_KEY = environ.get('GITHUB_API_KEY')
 GITHUB_ORG = environ.get('GITHUB_ORG')
 GITHUB_TEAM = environ.get('GITHUB_TEAM')
 CHECK_MODE = bool(environ.get('CHECK_MODE', True))
+
+log.setLevel(logging.DEBUG)
+
+log.debug("Starting")
+log.debug("Github Org: {github_org}".format(github_org=GITHUB_ORG))
+log.debug("Github Team: {github_team}".format(github_team=GITHUB_TEAM))
+log.debug("Check mode: {check_mode}".format(check_mode=CHECK_MODE))
 
 
 @app.route('/', methods=['POST'])
@@ -21,13 +31,13 @@ def refresh():
             "github_team": GITHUB_TEAM
         },
         check_mode=CHECK_MODE,
-        verbosity=0
+        verbosity=5
     )
     try:
         runner.run()
     except Exception as e:
         print(e)
-        abort(500, jsonify({'result': 'failed'}))
+        abort(500, jsonify({'result': 'failed', 'error': str(e)}))
     return make_response(jsonify({'result': 'success'}))
 
 
